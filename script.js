29d0b04d54c2ba0b6743d3c44891cd158e884a9f
MenukaUdayanga <ujayaweera738@gmail.com>
2023-06-04 22:23:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+function add(){
+
+    var firstNumber =parseInt(document.getElementById("first").value);
+    var secondNumber =parseInt(document.getElementById("second").value);
+
+    var answer =firstNumber+secondNumber;
+
+    document.getElementById("answer").innerHTML=answer;
+
+
+}
+
+function sub(){
+
+    var firstNumber =parseInt(document.getElementById("first").value);
+    var secondNumber =parseInt(document.getElementById("second").value);
+
+    var answer =firstNumber-secondNumber;
+
+    document.getElementById("answer").innerHTML=answer;
+
+
+}
+
+function divd(){
+
+    var firstNumber =parseInt(document.getElementById("first").value);
+    var secondNumber =parseInt(document.getElementById("second").value);
+
+    var answer =firstNumber/secondNumber;
+
+    document.getElementById("answer").innerHTML=answer;
+
+
+}
+
+function mul(){
+
+    var firstNumber =parseInt(document.getElementById("first").value);
+    var secondNumber =parseInt(document.getElementById("second").value);
+
+    var answer =firstNumber*secondNumber;
+
+    document.getElementById("answer").innerHTML=answer;
+
+
+}
+
